8a2b9c332a71bb370858d15eae5e0db40b1353c9
narasimhasv9318 <narasimha.mlops@gmail.com>
2026-03-08 20:15:15 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,35 @@
+// Edit this file to add, remove, or change products, prices, and images.
+// 
+// category options: 'cakes', 'brownies', 'donuts', 'tea-cakes'
+// price is in Indian Rupees (₹)
+
+const PRODUCT_DATA = [
+    {
+        id: 'cake-1',
+        name: 'Classic Chocolate Berry',
+        category: 'cakes',
+        price: 3500, // ₹3500
+        image: 'assets/gourmet_cake_1772978336876.png'
+    },
+    {
+        id: 'brownie-1',
+        name: 'Decadent Fudge Brownies (Box of 4)',
+        category: 'brownies',
+        price: 800, // ₹800
+        image: 'assets/fudge_brownies_1772978354574.png'
+    },
+    {
+        id: 'donut-1',
+        name: 'Assorted Gourmet Donuts (Box of 6)',
+        category: 'donuts',
+        price: 1200, // ₹1200
+        image: 'assets/assorted_donuts_1772978371743.png'
+    },
+    {
+        id: 'tea-cake-1',
+        name: 'Vanilla Pound Tea Cake',
+        category: 'tea-cakes',
+        price: 600, // ₹600
+        image: 'assets/tea_time_cake_1772978388914.png'
+    }
+];
